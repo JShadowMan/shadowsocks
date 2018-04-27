8df82a919c99b59dbfd838d7e025e0674e140905
Jayson Wang <shadowman@shellboot.com>
2018-04-27 21:57:36 +0800

    convert crlf to lf

--- src/server/ss_server.cc
+++ src/server/ss_server.cc
@@ -1,21 +1,23 @@
-#include <iostream>
 #include "shadowsocks/ss_network.h"
 #include "shadowsocks/server/ss_server.h"
 
 
 // Ss_Server constructor
 Ss_Server::Ss_Server()
-    : _network(new Ss_Network()) {
+    : _tcp_server(Ss_Network::generateTcpMetwork()),
+      _udp_server(Ss_Network::generateUdpNetwork()) {
 }
 
 // destructor of Ss_Server
 Ss_Server::~Ss_Server() {
-    delete _network;
+    delete _tcp_server;
+    delete _udp_server;
 }
 
 // create daemon and start listening
 bool Ss_Server::createDaemon() {
-    _network->listen("0.0.0.0", 9980);
+    _tcp_server->listen("0.0.0.0", 9980);
+    _udp_server->listen("0.0.0.0", 9980);
 
     return true;
 }
