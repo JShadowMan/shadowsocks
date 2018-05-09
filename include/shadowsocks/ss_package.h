4cbfc152116903804acd97949b673cdb9d845d15
JShadowMan <shadowman@shellboot.com>
2018-05-09 22:25:45 +0800

    package parse class

---
+++ include/shadowsocks/ss_package.h
@@ -0,0 +1,17 @@
+#ifndef __SHADOWSOCKS_PACKAGE_INCLUDED__
+#define __SHADOWSOCKS_PACKAGE_INCLUDED__
+
+
+// other shadowsocks headers
+#include "ss_types.h"
+
+
+/* `Ss_Package` class */
+class Ss_Package {
+    public:
+        explicit Ss_Package(u_char *data);
+};
+
+
+
+#endif // __SHADOWSOCKS_PACKAGE_INCLUDED__
---
+++ src/lib/ss_package.cc
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "shadowsocks/ss_package.h"
+
+
+// Ss_package constructor
+Ss_Package::Ss_Package(u_char *data) {
+    std::cout << data << std::endl;
+}
